c42946a2e7448bc4ebd33331fbb016c72256a1d2
tiarnola <tiarnola@gmail.com>
2021-11-17 23:17:42 +0000

    push

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+
 import './App.css';
-import axios from 'axios';
+
 function App() {
-
- 
-
-
-
-
   return (
     <div className="App">
-      <h1>APIIIIII</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
